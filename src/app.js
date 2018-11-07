ae9a12e06a515cb1330a45a64c0d007ee2e0e69c
cukeyb <yangbing1017@12.com>
2018-11-07 22:06:31 +0800

    new tabBar

--- src/app.js
+++ src/app.js
@@ -7,14 +7,43 @@ class App extends Component {
 
   config = {
     pages: [
-      'pages/index/index'
+      'pages/index/index',
+      "pages/logs/logs",
+      "pages/my/my"
     ],
     window: {
       backgroundTextStyle: 'light',
       navigationBarBackgroundColor: '#fff',
-      navigationBarTitleText: 'WeChat',
+      navigationBarTitleText: '商城',
       navigationBarTextStyle: 'black'
-    }
+    },
+    tabBar: {  
+      "color": "#a9b7b7",  
+      "selectedColor": "#11cd6e",  
+      "borderStyle": "black" ,
+      "list": [{  
+        "selectedIconPath": "images/index2.png",  
+        "iconPath": "images/index1.png",  
+        "pagePath": "pages/index/index",  
+        "text": "首页"  
+      }, {  
+        "selectedIconPath": "images/classify2.png",  
+        "iconPath": "images/classify1.png",  
+        "pagePath": "pages/logs/logs",  
+        "text": "分类"  
+      },{  
+        "selectedIconPath": "images/buycar2.png",  
+        "iconPath": "images/buycar1.png",  
+        "pagePath": "pages/my/my",  
+        "text": "购物车"  
+      },{  
+        "selectedIconPath": "images/my2.png",  
+        "iconPath": "images/my1.png",  
+        "pagePath": "pages/my/my",  
+        "text": "我的"  
+      }
+      ]  
+    } 
   }
 
   componentDidMount () {}
